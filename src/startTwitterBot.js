0add22ba593d0148783d2bb4dbc1dbfd524868bc
Toti Muñoz <gonzalo.munoz@southworks.com>
2021-04-29 20:29:14 -0300

    ✨ Add charts images to tweets

--- src/startTwitterBot.js
+++ src/startTwitterBot.js
@@ -2,21 +2,33 @@ const Twit = require("twit");
 const utils = require("./utils");
 const config = require("./config");
 const createUpdatedMessage = require("./createUpdatedMessage");
+const axios = require("axios").default;
 
 let lastPrice;
 
+const createMedia = async (base64, Twitter) =>
+    (await Twitter.post("media/upload", { media_data: base64 }))
+        .data.media_id_string;
+
 const tweet = async (coin, Twitter) => {
-    const messageData = await createUpdatedMessage(coin, lastPrice);
+    const { message, price } = await createUpdatedMessage(coin, lastPrice);
 
-    lastPrice = messageData.price;
+    lastPrice = price;
 
-    Twitter.post(
-        "statuses/update",
-        { status: messageData.message },
-        (err, data, response) => {
-            console.log(data);
-        }
+    const url = `${config.charts_api_url}/candlestick/binance?coin=btc&interval=1m&limit=31`;
+    const b64 = (await axios.get(url)).data.base64.replace(
+        "data:image/png;base64,",
+        ""
     );
+
+    const mediaId = await createMedia(b64, Twitter);
+
+    Twitter.post("statuses/update", {
+        status: message,
+        media_ids: [mediaId],
+    })
+        .then(({ data }) => console.log("Tweeted at", data.created_at))
+        .catch((error) => console.error(error));
 };
 
 const startTwitterBot = (coin, intervalMinutes) => {
